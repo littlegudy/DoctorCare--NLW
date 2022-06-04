7839b181f04d5f54018b33d118eaf1320e725070
Luis Claudio Jr <luis_rocha1999@outlook.com>
2022-06-04 02:36:15 -0300

    responsivo para desktop

--- main.js
+++ main.js
@@ -4,7 +4,41 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
 }
+
+
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    //verificar se a secao passou da linha
+    // quais dados precisarei?
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    //verificar se a base está abaixo da linha alvo
+    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+
+    //limites da secao
+    const sectionBoundaries = 
+    sectionTopReachOrPassedTargetLine &&  !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if(sectionBoundaries) {
+        menuElement.classList.add('active')
+    }   
+} 
+
+
 
 function showNavOnScroll() {
     if (scrollY > 0) {
@@ -30,6 +64,8 @@ function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
 
+
+
 ScrollReveal({
     origin: 'top',
     distance: '30px',
@@ -45,3 +81,5 @@ ScrollReveal({
   #about header,
   #about .content`);
 
+
+  
